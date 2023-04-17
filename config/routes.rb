e0c55acf8bc22bfdb4e9517ab9cc7416c27bc32b
Kadoadmin <shumeeruin@outlook.jp>
2023-04-17 11:54:41 +0900

    create admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"}
   root to: "homes#top" 
   get '/about' => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create!(
+  name: "管理人",
+  email: "shumeeruin@outlook.jp",
+  password: "kamemushi",
+ )
